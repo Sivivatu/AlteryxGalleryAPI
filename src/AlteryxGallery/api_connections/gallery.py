2e136f965f2dea20b1de16ef7d4d606c5af36f08
Paul Houghton <paul.j.houghton@gmail.com>
2021-11-17 20:14:01 +0000

    refactor gallery calls

---
+++ src/AlteryxGallery/api_connections/gallery.py
@@ -0,0 +1,108 @@
+import json
+import requests
+from AlteryxGallery.gallery_connection import Gallery
+
+class GalleryUser(Gallery):
+    '''Extends the Gallery Class containing connection to access Alteryx Server API endpoints'''
+    def __init__(self, api_location: str, api_key: str, api_secret: str):
+        super().__init__(self.api_location, self.api_key, self.api_secret)
+
+    def subscription(self):
+        """
+        :return: workflows in a subscription
+        """
+        method = 'GET'
+        url = self.api_location + '/workflows/subscription/'
+        params = self.build_oauth_params()
+        signature = self.generate_signature(method, url, params)
+        params.update({'oauth_signature': signature})
+        response = requests.get(url, params=params)
+        output, output_content = response, json.loads(response.content.decode("utf8"))
+        return output, output_content
+
+    def questions(self, app_id):
+        """
+        :return: Returns the questions for the given Alteryx Analytics App
+        """
+        method = 'GET'
+        url = self.api_location + '/workflows/' + app_id + '/questions/'
+        params = self.build_oauth_params()
+        signature = self.generate_signature(method, url, params)
+        params.update({'oauth_signature': signature})
+        response = requests.get(url, params=params)
+        output, output_content = response, json.loads(response.content.decode("utf8"))
+        return output, output_content
+
+    def execute_workflow(self, app_id, **kwargs):
+        """
+        Queue an app execution job.
+        :return:  Returns ID of the job
+        """
+        method = 'POST'
+        url = self.api_location + '/workflows/' + app_id + '/jobs/'
+        params = self.build_oauth_params()
+        signature = self.generate_signature(method, url, params)
+        params.update({'oauth_signature': signature})
+
+        if 'payload' in kwargs:
+            output = requests.post(url,
+                                json=kwargs['payload'],
+                                headers={'Content-Type': 'application/json'},
+                                params=params)
+        else:
+            response = requests.post(url, params=params)
+
+        output, output_content = response, json.loads(response.content.decode("utf8"))
+        return output, output_content
+
+    def get_jobs(self, app_id):
+        """
+        :return: Returns the jobs for the given Alteryx Analytics App
+        """
+        method = 'GET'
+        url = self.api_location + '/workflows/' + app_id + '/jobs/'
+        params = self.build_oauth_params()
+        signature = self.generate_signature(method, url, params)
+        params.update({'oauth_signature': signature})
+        response = requests.get(url, params=params)
+        output, output_content = response, json.loads(response.content.decode("utf8"))
+        return output, output_content
+
+    def get_job_status(self, job_id):
+        """
+        :return: Retrieves the job and its current state
+        """
+        method = 'GET'
+        url = self.api_location + '/jobs/' + job_id + '/'
+        params = self.build_oauth_params()
+        signature = self.generate_signature(method, url, params)
+        params.update({'oauth_signature': signature})
+        response = requests.get(url, params=params)
+        output, output_content = response, json.loads(response.content.decode("utf8"))
+        return output, output_content
+
+    def get_job_output(self, job_id, output_id):
+        """
+        :return: Returns the output for a given job (FileURL)
+        """
+        method = 'GET'
+        url = self.api_location + '/jobs/' + job_id + '/output/' + output_id + '/'
+        params = self.build_oauth_params()
+        signature = self.generate_signature(method, url, params)
+        params.update({'oauth_signature': signature})
+        response = requests.get(url, params=params)
+        output, output_content = response, response.content.decode("utf8")
+        return output, output_content
+
+    def get_app(self, app_id):
+        """
+        :return: Returns the App that was requested
+        """
+        method = 'GET'
+        url = self.api_location + '/' + app_id + '/package/'
+        params = self.build_oauth_params()
+        signature = self.generate_signature(method, url, params)
+        params.update({'oauth_signature': signature})
+        response = requests.get(url, params=params)
+        output, output_content = response, json.loads(response.content.decode("utf8"))
+        return output, output_content
