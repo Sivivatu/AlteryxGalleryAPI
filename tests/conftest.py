28d55e536ba6c09cdd78c9b60bdfe2e78dac756f
Paul <Sivivatu@users.noreply.github.com>
2021-11-26 09:20:35 +0000

    corrected gallery call for breaking change

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from decouple import config
 
 
 from Alteryx_Gallery.api_connections.admin import GalleryAdmin
-from Alteryx_Gallery.api_connections.gallery import GalleryUser
+from Alteryx_Gallery.api_connections.subscription import GallerySubscription
 
 
 @pytest.fixture(scope="session")
@@ -12,7 +12,7 @@ def api_con():
     client_secret = config('client_secret')
     apiLocation = config('gallery_url')
 
-    with GalleryUser(apiLocation, client_secret, client_key) as test_conn:
+    with GallerySubscription(apiLocation, client_secret, client_key) as test_conn:
         return test_conn
 
 
