46de05b53a39662dc8c73a1ecd5c45245295d318
Paul Houghton <paul.j.houghton@gmail.com>
2024-04-04 16:56:49 +0100

    local testing script

--- scratch.py
+++ scratch.py
@@ -1,19 +1,17 @@
 import os
-from dotenv import load_dotenv
+from pathlib import Path
+
 from AlteryxGallery import AlteryxGalleryAPI
+from dotenv import load_dotenv
 
 load_dotenv()
 
 base_url = "https://spider.theinformationlab.co.uk/webapi/"
 
-# token_url = f"{base_url}oauth2/token"
-
-# url = "https://spider.theinformationlab.co.uk/webapi/v3/workflows?view=Default"
-
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-
-
+client_id: str = os.getenv("CLIENT_ID", "NoValueFound")
+client_secret: str = os.getenv("CLIENT_SECRET", "NoValueFound")
+owner_id: str = os.getenv("TEST_OWNER_ID", "NoValueFound")
+file_path = Path("tests/Test_Upload.yxzp")
 # payload = {"client_id": client_id, "client_secret": client_secret, "grant_type": "client_credentials"}
 
 # response = httpx.post(token_url, data=payload)
@@ -25,6 +23,9 @@ client_secret = os.getenv("CLIENT_SECRET")
 
 with AlteryxGalleryAPI.GalleryClient(base_url=base_url) as client:
     client.authenticate(client_id, client_secret)
-    response, content = client.get_all_workflows(name="00-Octopus Download Pipeline")
-    print(f"workflow query status response: {response.status_code}")
-    print(f"workflow query content: {content}")
+    # response, content = client.get_all_workflows(name="00-Octopus Download Pipeline")
+    # print(f"workflow query status response: {response.status_code}")
+    # print(f"workflow query content: {content}")
+    response, content = client.post_publish_workflow(file_path, "Test_Upload", owner_id)
+    print(f"workflow publish query status response: {response.status_code}")
+    print(f"workflow publish query content: {content}")
